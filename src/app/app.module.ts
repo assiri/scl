7111993fb30dba82d9f83ba5c3013c85b04ca7e3
aissri <assiri@hotmail.com.com>
2020-02-22 17:38:40 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { ResultsComponent } from './results/results.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    NavbarComponent,
+    QuestionsComponent,
+    ResultsComponent,
+    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
